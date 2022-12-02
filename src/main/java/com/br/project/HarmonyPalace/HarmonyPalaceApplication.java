b9a95e07200fb2a845f0c191a190369c24bf31f3
lucasn07 <lucasn0796@gmail.com>
2022-12-02 20:15:08 -0300

    Inicio do projeto / back-end

---
+++ src/main/java/com/br/project/HarmonyPalace/HarmonyPalaceApplication.java
@@ -0,0 +1,13 @@
+package com.br.project.HarmonyPalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonyPalaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonyPalaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/project/HarmonyPalace/HarmonyPalaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.project.HarmonyPalace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyPalaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
