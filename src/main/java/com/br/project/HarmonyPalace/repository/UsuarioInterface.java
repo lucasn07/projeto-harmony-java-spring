cecdc138aeb54622dab4126bfb6b5642e00794ef
lucasn07 <lucasn0796@gmail.com>
2022-12-07 14:57:33 -0300

    alterações simples apenas para testes

--- src/main/java/com/br/project/HarmonyPalace/repository/UsuarioInterface.java
+++ src/main/java/com/br/project/HarmonyPalace/repository/UsuarioInterface.java
@@ -1,8 +1,6 @@
 package com.br.project.HarmonyPalace.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
 import com.br.project.HarmonyPalace.entities.Usuario;
 
 public interface UsuarioInterface extends JpaRepository<Usuario, Integer> {
